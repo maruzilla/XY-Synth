e09015889ade2b11fffeb31e77b093a9fad3cb8a
Err-Tripper <46555652+maruzilla@users.noreply.github.com>
2026-03-04 16:08:19 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'synth-v1';
+const CACHE_NAME = 'synth-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -7,6 +7,7 @@ const ASSETS = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
+    self.skipWaiting(); // 新しいSWをすぐに有効化
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
